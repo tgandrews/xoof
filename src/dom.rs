4384723b8cb03f455e00915a49c9c82d5a4a017c
Tom Andrews <git@tomandrews.co.uk>
2016-08-22 11:43:06 +0100

    Dom tree pretty printing :D

--- src/dom.rs
+++ src/dom.rs
@@ -1,33 +1,63 @@
 use std::collections::{HashMap,HashSet};
+use std::fmt;
 
-struct Node {
-    children: Vec<Node>,
-    node_type: NodeType,
-}
-
+#[derive(Debug)]
 enum NodeType {
     Text(String),
     Element(ElementData),
 }
 
+#[derive(Debug)]
+pub struct Node {
+    pub children: Vec<Node>,
+    node_type: NodeType,
+}
+
+impl fmt::Display for Node {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.print(0))
+    }
+}
+
+impl Node {
+    fn print(&self, depth: usize) -> String {
+        let tag = match self.node_type {
+            NodeType::Text(_) => "Text".to_string(),
+            NodeType::Element(ref elem) => elem.tag_name.clone()
+        };
+        let mut indent = String::new();
+        for _ in 0..depth {
+            indent.push_str("  ");
+        }
+        let mut children_output = String::new();
+        let next_depth = depth + 1;
+        for child in &self.children {
+            children_output += child.print(next_depth).as_str();
+            children_output += "\n";
+        }
+        return indent + tag.as_str() + "\n" + children_output.as_str();
+    }
+}
+
+#[derive(Debug)]
 struct ElementData {
     tag_name: String,
     attributes: AttrMap,
 }
 
-type AttrMap = HashMap<String, String>;
+pub type AttrMap = HashMap<String, String>;
 
 
-fn text(data: String) -> Node {
+pub fn text(data: String) -> Node {
     Node { children: Vec::new(), node_type: NodeType::Text(data) }
 }
 
-fn elem(name: String, attrs, AttrMap, children: Vec<Node>) -> Node {
+pub fn element(name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
     Node {
         children: children,
         node_type: NodeType::Element(ElementData {
             tag_name: name,
-            attributes: attr,
+            attributes: attrs,
         })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+pub mod dom;
+
 fn main() {
-    println!("Hello, world!");
+    let text = dom::text("Hello, world!".to_string());
+    let body = dom::element("body".to_string(), dom::AttrMap::new(), vec!(text));
+    let root = dom::element("html".to_string(), dom::AttrMap::new(), vec!(body));
+    println!("Tree: \n{}", root);
 }
