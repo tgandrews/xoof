55220d1e01cc956562d1365512f2fbd717a01381
Tom Andrews <git@tomandrews.co.uk>
2016-08-23 15:14:33 +0100

    Fix warning by removing braces

--- src/html_parser_test.rs
+++ src/html_parser_test.rs
@@ -5,7 +5,7 @@ use dom;
 fn it_works() {
     match parse("<test></test>".to_string()) {
         Ok(node) => {
-            match (node.node_type) {
+            match node.node_type {
                 dom::NodeType::Element(e) => assert_eq!(e.tag_name, "test"),
                 _ => assert!(false, "Wrong node type")
             }
